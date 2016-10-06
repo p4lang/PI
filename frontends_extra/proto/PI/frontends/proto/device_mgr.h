7a0a2f98e3526a372f1b9b3d79202dc5a6fdab2e
Antonin Bas <antonin@barefootnetworks.com>
2016-10-06 16:36:43 -0700

    Add interface for tentative PI Protobuf frontend
    This frontend translates Protobuf messages to PI C API calls, thus
    removing the burden from the gRPC server. The gRPC server still has to
    handle batching (e.g. for table operations) and streaming (e.g. for
    packet punting). Note that packet in / out calls handle raw bytes
    only. CPU header encapsulation has to be done either at the controller
    or gRPC server.

---
+++ frontends_extra/proto/PI/frontends/proto/device_mgr.h
@@ -0,0 +1,85 @@
+/* Copyright 2013-present Barefoot Networks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Antonin Bas (antonin@barefootnetworks.com)
+ *
+ */
+
+#ifndef PI_FRONTENDS_PROTO_DEVICE_MGR_H_
+#define PI_FRONTENDS_PROTO_DEVICE_MGR_H_
+
+#include "google/rpc/status.pb.h"
+#include "pi.pb.h"
+
+#include <functional>
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace pi {
+
+namespace fe {
+
+namespace proto {
+
+// forward declaration for PIMPL class
+class DeviceMgrImp;
+
+// the gRPC server will instantiate one DeviceMgr object per device
+class DeviceMgr {
+ public:
+  using device_id_t = uint64_t;
+  using p4_id_t = uint32_t;
+  // may change when we introduce specific error namespace
+  using Status = ::google::rpc::Status;
+  using PacketInCb =
+      std::function<void(device_id_t, std::string packet, void *cookie)>;
+
+  DeviceMgr(device_id_t device_id);
+
+  ~DeviceMgr();
+
+  // should we use ::google::rpc::Status or should we just return an error code
+  // that the gRPC server can then wrap in a Status message
+  Status table_write(const p4::TableUpdate &table_update);
+
+  std::vector<p4::TableEntry> table_read(p4_id_t table_id) const;
+
+  Status action_profile_write(
+      const p4::ActionProfileUpdate &action_profile_update);
+
+  std::vector<p4::ActionProfileEntry> action_profile_read(
+      p4_id_t action_profile_id) const;
+
+  // from the perspective of P4, a punted packet is just bytes. Either the
+  // controller is responsible for encapsulating the packet in the appropriate
+  // header, or the gRPC server is.
+  Status packet_out_send(const std::string &packet) const;
+
+  void packet_in_register_cb(PacketInCb cb, void *cookie);
+
+ private:
+  // PIMPL design
+  std::unique_ptr<DeviceMgrImp> pimp;
+};
+
+}  // namespace proto
+
+}  // namespace fe
+
+}  // namespace pi
+
+#endif  // PI_FRONTENDS_PROTO_DEVICE_MGR_H_
---
+++ frontends_extra/proto/src/device_mgr.cpp
@@ -0,0 +1,127 @@
+/* Copyright 2013-present Barefoot Networks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Antonin Bas (antonin@barefootnetworks.com)
+ *
+ */
+
+#include <PI/frontends/proto/device_mgr.h>
+
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace pi {
+
+namespace fe {
+
+namespace proto {
+
+using device_id_t = DeviceMgr::device_id_t;
+using p4_id_t = DeviceMgr::p4_id_t;
+using Status = DeviceMgr::Status;
+using PacketInCb = DeviceMgr::PacketInCb;
+
+class DeviceMgrImp {
+ public:
+  DeviceMgrImp(device_id_t device_id)
+      : device_id(device_id) { }
+
+  // TODO(antonin)
+  Status table_write(const p4::TableUpdate &table_update) {
+    (void) table_update;
+    return Status();
+  }
+
+  // TODO(antonin)
+  std::vector<p4::TableEntry> table_read(p4_id_t table_id) const {
+    (void) table_id;
+    return {};
+  }
+
+  // TODO(antonin)
+  Status action_profile_write(
+      const p4::ActionProfileUpdate &action_profile_update) {
+    (void) action_profile_update;
+    return Status();
+  }
+
+  // TODO(antonin)
+  std::vector<p4::ActionProfileEntry> action_profile_read(
+      p4_id_t action_profile_id) const {
+    (void) action_profile_id;
+    return {};
+  }
+
+  // TODO(antonin)
+  Status packet_out_send(const std::string &packet) const {
+    (void) packet;
+    return Status();
+  }
+
+  // TODO(antonin)
+  void packet_in_register_cb(PacketInCb cb, void *cookie) {
+    (void) cb; (void) cookie;
+  }
+
+ private:
+  device_id_t device_id;
+};
+
+DeviceMgr::DeviceMgr(device_id_t device_id) {
+  pimp = std::unique_ptr<DeviceMgrImp>(new DeviceMgrImp(device_id));
+}
+
+DeviceMgr::~DeviceMgr() { }
+
+// PIMPL forwarding
+
+Status
+DeviceMgr::table_write(const p4::TableUpdate &table_update) {
+  return pimp->table_write(table_update);
+}
+
+std::vector<p4::TableEntry>
+DeviceMgr::table_read(p4_id_t table_id) const {
+  return pimp->table_read(table_id);
+}
+
+Status
+DeviceMgr::action_profile_write(
+    const p4::ActionProfileUpdate &action_profile_update) {
+  return pimp->action_profile_write(action_profile_update);
+}
+
+std::vector<p4::ActionProfileEntry>
+DeviceMgr::action_profile_read(p4_id_t action_profile_id) const {
+  return pimp->action_profile_read(action_profile_id);
+}
+
+Status
+DeviceMgr::packet_out_send(const std::string &packet) const {
+  return pimp->packet_out_send(packet);
+}
+
+void
+DeviceMgr::packet_in_register_cb(PacketInCb cb, void *cookie) {
+  return pimp->packet_in_register_cb(cb, cookie);
+}
+
+}  // namespace proto
+
+}  // namespace fe
+
+}  // namespace pi
