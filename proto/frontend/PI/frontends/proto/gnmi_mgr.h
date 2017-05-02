1279789f5240c88cdd116352d1bc9fd413c39838
Antonin Bas <antonin@barefootnetworks.com>
2017-05-02 10:49:38 -0700

    Took a stab at a GnmiMgr
    Unlike DeviceMgr, GnmigMgr manages multiple devices because of the way the gnmi
    proto was written. We need to update DeviceMgr to also manage multiple devices
    for consistency. In the future we can also merge the 2 into one class, but for
    now we keep them separate for the sake of simplicity.
    
    Just like DeviceMgr, GnmiMgr uses the PIMPL idiom. We do not provide an
    implementation of it though, it has to be provided by the user at link
    time. This is also subject to change.
    
    All this code is more a POC than anything else at this stage.

---
+++ proto/frontend/PI/frontends/proto/gnmi_mgr.h
@@ -0,0 +1,62 @@
+/* Copyright 2013-present Barefoot Networks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Antonin Bas (antonin@barefootnetworks.com)
+ *
+ */
+
+#ifndef PI_FRONTENDS_PROTO_GNMI_MGR_H_
+#define PI_FRONTENDS_PROTO_GNMI_MGR_H_
+
+#include <memory>
+
+#include "gnmi/gnmi.pb.h"
+#include "google/rpc/status.pb.h"
+
+namespace pi {
+
+namespace fe {
+
+namespace proto {
+
+// forward declaration for PIMPL class
+class GnmiMgrImp;
+
+class GnmiMgr {
+ public:
+  using Status = ::google::rpc::Status;
+
+  GnmiMgr();
+  ~GnmiMgr();
+
+  Status config_get(const gnmi::GetRequest &request,
+                    gnmi::GetResponse *response) const;
+
+  Status config_set(const gnmi::SetRequest &request,
+                    gnmi::SetResponse *response);
+
+ private:
+  // PIMPL design
+  std::unique_ptr<GnmiMgrImp> pimp;
+};
+
+}  // namespace proto
+
+}  // namespace fe
+
+}  // namespace pi
+
+#endif  // PI_FRONTENDS_PROTO_GNMI_MGR_H_
