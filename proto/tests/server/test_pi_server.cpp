079160172ccdc6e3e70f5bdc113228b743096c5e
dushyantarora13 <dushyantarora@google.com>
2017-11-28 00:38:39 -0800

    Fixed style error

--- proto/tests/server/test_pi_server.cpp
+++ proto/tests/server/test_pi_server.cpp
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <string>
 #include <gtest/gtest.h>
 #include <PI/proto/pi_server.h>
 
