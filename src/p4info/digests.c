0697385b1f5c37a0db3b915215ef65a6ae16d5d0
Andy Fingerhut <andy.fingerhut@gmail.com>
2025-02-01 23:15:01 -0500

    Add SPDX license id lines to source files
    Signed-off-by: Andy Fingerhut <andy.fingerhut@gmail.com>

--- src/p4info/digests.c
+++ src/p4info/digests.c
@@ -1,5 +1,6 @@
 /* Copyright 2018-present Barefoot Networks, Inc.
  * Copyright 2021 VMware, Inc.
+ * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
